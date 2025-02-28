5fe2fe5001d72c45a2192739e60d1f1f0ae947b9
wojnarek <wojnarek99@gmail.com>
2025-02-28 18:06:20 +0100

    Added ansible files

---
+++ ansible/roles/iot/files/code/get_sensor_data.py
@@ -0,0 +1,46 @@
+import time
+import board
+import adafruit_dht
+
+dht_device = adafruit_dht.DHT11(board.D4)
+
+previous_temperature = None
+previous_humidity = None
+
+def get_sensor_data():
+    global previous_temperature, previous_humidity
+    try:
+        
+        temperature = dht_device.temperature
+        humidity = dht_device.humidity
+
+        
+        if temperature is not None and humidity is not None:
+            previous_temperature = temperature
+            previous_humidity = humidity
+            return temperature, humidity
+        else:
+            
+            if previous_temperature is not None and previous_humidity is not None:
+                return previous_temperature, previous_humidity
+            else:
+                
+                return None, None
+
+    except RuntimeError as e:
+        
+        print(f"Sensor error: {e}")
+        if previous_temperature is not None and previous_humidity is not None:
+            return previous_temperature, previous_humidity
+        else:
+            
+            return None, None
+
+if __name__ == "__main__":
+    while True:
+        t, h = get_sensor_data()
+        if t is not None and h is not None:
+            print(f"Temperature: {t}°C  Humidity: {h}%")
+        else:
+            print("Sensor data error")
+        time.sleep(2)
